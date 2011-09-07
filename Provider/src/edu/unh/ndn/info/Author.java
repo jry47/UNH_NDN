95a18a8e96099be292088d7fc2ebc048e8af226e
ryangoulding <rdb23@wildcats.unh.edu>
2011-09-07 13:04:31 -0700

    added a comment to Author.java

--- Provider/src/edu/unh/ndn/info/Author.java
+++ Provider/src/edu/unh/ndn/info/Author.java
@@ -1,5 +1,5 @@
 package edu.unh.ndn.info;
-/*a comment for assignment #1 which can later be removed*/
+/*a comment for assignment #1 which can later be remove d*/
 public interface Author {
 	String getAuthor();
 }
