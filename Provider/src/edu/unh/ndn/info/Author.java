dcad05bd38d5c9d282f2f337e9d835bf79a3900d
John Read Stuart <jry47@pacw1.cs.unh.edu>
2011-09-07 17:53:28 -0400

    i commited something...

--- Provider/src/edu/unh/ndn/info/Author.java
+++ Provider/src/edu/unh/ndn/info/Author.java
@@ -1,5 +1,7 @@
 package edu.unh.ndn.info;
 /*a comment for assignment #1 which can later be removed*/
+
+/*i'm making a comment!!!!!! -john stuart*/
 public interface Author {
-	String getAuthor();
+ String getAuthor();
 }
